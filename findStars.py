e2575f025cf78c2cf7656eec3b45d0080c8c6f2a
chopley <chopley@gmail.com>
2015-07-30 09:56:54 +0200

    added

--- findStars.py
+++ findStars.py
@@ -3,24 +3,30 @@
 #1) Start at a given time, latitude, longitude, elevation, azimuth, field of view
 #2) Find the next star that will pass through the elevation,azimuth, as well as the time it will pass through
 #3) Output the star, and the time it will pass through transit
+#4) Grab images from the frame grabber for <TBD> time before and after zenith transit
+#Written by Charles Copley,AVN Science
 
 #Call as follows
 # python findStars.py 90 2 2015/8/21 Kuntunse South (where 90 is the elevation position and 2 is the field of view of the camera)
 # This will return a list of appropriate stars for the Ghana Dish, RA, DEC, time of transit, elevation angle at transit
+
 #Rev 1.0 29/7/2015 
 #TO DO: 
 #1) Add appropriate functionality for location and time/date (probably a text file with location names and lat/long/altitude).
 #2) Also work out if the transit is North or South facing (pretty simple)
 #3) Add function to take images with the frame grabber during the transit?
 #4) 
+
 #Rev 2.0 29/7/2015 
 #Done
 #1) Added a text file (sites.txt) that is read to get appropriate details for the site location.
+#2) Added a North/South option to allow you to select which side of zenith the star will be
+
 #TO DO: 
 #1) Also work out if the transit is North or South facing (pretty simple)
 #2) Add function to take images with the frame grabber during the transit?
 
-#Written by Charles Copley 28/7/2015
+
 import pandas,numpy,ephem,sys
 #define the column layout
 
@@ -35,8 +41,6 @@ siteName = sys.argv[4]
 direction = sys.argv[5]
 
 
-elevationRadians = numpy.deg2rad(float(elevation))
-azimuth = 180
 
 
 #read in the list of sites
@@ -52,11 +56,12 @@ starList['DECd']=""
 starList['SkyAngle']=""
 starList['TransitTime']=""
 
-#Set up the observing site
+#Set up the observing site using the information from the text file
 Kuntunse=ephem.Observer()
 Kuntunse.lat=(siteDetail['Lat'])*ephem.degree
 Kuntunse.lon=(siteDetail['Lon'])*ephem.degree
-Kuntunse.date = date #get the date from the input argument earlier 
+#get the date from the input argument earlier 
+Kuntunse.date = date 
 
 print Kuntunse.lat,Kuntunse.lon,elevation
 
@@ -73,21 +78,17 @@ for i in range(0,len(starList)):
 	DirectionStr='North'
 	if(Direction<0):	
 		DirectionStr='South'
-	#starList['TransitTime'][i] = ephem.Date.datetime(Kuntunse.next_transit(source))
 	#Need to check if the source will ever be up
 	if(~source.neverup):
 		try:
 			zenithAngle = numpy.rad2deg(numpy.deg2rad(90) - source.transit_alt.norm	)
-			aa = '%s %s %s %s %s %s %s' %(starList['Name'][i],starList['RA'][i],starList['DEC'][i],source.transit_time,source.transit_alt.norm,DirectionStr,zenithAngle)
+			outputString = '%s\t%s\t%s\t%s\t%s\t%s\t%2.2f' %(starList['Name'][i],starList['RA'][i],starList['DEC'][i],source.transit_time,source.transit_alt.norm,DirectionStr,zenithAngle)
 			if(source.transit_alt.norm>numpy.radians(elevation-fieldOfView) and  (source.transit_alt.norm<numpy.radians(elevation+fieldOfView))):
 				if(DirectionStr==direction):
-					print aa
+					print outputString
 		except:
-			aa = '%s %s %s' %(starList['Name'][i],starList['RA'][i],starList['DEC'][i])
+			outputString = '%s %s %s' %(starList['Name'][i],starList['RA'][i],starList['DEC'][i])
 
-	#print(Kuntunse.next_transit(source))
-	#print(source.transit_alt.norm)
-	#rint(source._dec)
 
 
 
